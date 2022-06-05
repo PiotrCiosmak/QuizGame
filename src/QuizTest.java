b7d1bbefc2ce60802ba722fac9e245ca9f3ee049
Piotr Ciosmak <piotrciosmak2001@gmail.com>
2022-06-05 13:15:22 +0200

    added a few unit tests

---
+++ src/QuizTest.java
@@ -0,0 +1,107 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class QuizTest
+{
+    @Test
+    void checkWidthOfButtonA()
+    {
+        Quiz quiz = new Quiz();
+        assertEquals(100, quiz.buttonA.getWidth());
+    }
+
+    @Test
+    void checkHeightOfButtonA()
+    {
+        Quiz quiz = new Quiz();
+        assertEquals(100, quiz.buttonA.getHeight());
+    }
+
+    @Test
+    void checkTextOfButtonA()
+    {
+        Quiz quiz = new Quiz();
+        assertEquals("A", quiz.buttonA.getText());
+    }
+
+    @Test
+    void checkWidthOfButtonB()
+    {
+        Quiz quiz = new Quiz();
+        assertEquals(100, quiz.buttonB.getWidth());
+    }
+
+    @Test
+    void checkHeightOfButtonB()
+    {
+        Quiz quiz = new Quiz();
+        assertEquals(100, quiz.buttonB.getHeight());
+    }
+
+    @Test
+    void checkTextOfButtonB()
+    {
+        Quiz quiz = new Quiz();
+        assertEquals("B", quiz.buttonB.getText());
+    }
+
+    @Test
+    void checkWidthOfButtonC()
+    {
+        Quiz quiz = new Quiz();
+        assertEquals(100, quiz.buttonC.getWidth());
+    }
+
+    @Test
+    void checkHeightOfButtonC()
+    {
+        Quiz quiz = new Quiz();
+        assertEquals(100, quiz.buttonC.getHeight());
+    }
+
+    @Test
+    void checkTextOfButtonC()
+    {
+        Quiz quiz = new Quiz();
+        assertEquals("C", quiz.buttonC.getText());
+    }
+
+    @Test
+    void checkWidthOfButtonD()
+    {
+        Quiz quiz = new Quiz();
+        assertEquals(100, quiz.buttonD.getWidth());
+    }
+
+    @Test
+    void checkHeightOfButtonD()
+    {
+        Quiz quiz = new Quiz();
+        assertEquals(100, quiz.buttonD.getHeight());
+    }
+
+    @Test
+    void checkTextOfButtonD()
+    {
+        Quiz quiz = new Quiz();
+        assertEquals("D", quiz.buttonD.getText());
+    }
+
+    @Test
+    void checkIfSecondEqualsTen()
+    {
+        Quiz quiz = new Quiz();
+        assertEquals(10, quiz.seconds);
+    }
+
+    @Test
+    void checkIfPercentageIs100WhenAllAnswersCorrect()
+    {
+        Quiz quiz = new Quiz();
+        if (quiz.correct_guesses== quiz.total_questions)
+        {
+            assertEquals("100", quiz.percentage.getText());
+        }
+    }
+}
